ef397be07109c6d33d3512c5786c57fdebbfcd12
ManohariAU <117429123+ManohariAU@users.noreply.github.com>
2023-07-28 09:54:16 +0530

    Delete src directory

--- src/main/java/com/iamneo/security/entity/Role.java
+++
@@ -1,9 +0,0 @@
-package com.iamneo.security.entity;
-
-public enum Role {
-   ADMIN,
-   TEACHER,
-   STUDENT;
-    public static final Role DEFAULT_ROLE = STUDENT; // Set STUDENT as the default role
-
-}
--- src/src/main/java/com/iamneo/security/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.iamneo.security.entity;
-
-public enum Role {
-    USER,
-    TEAM_LEADER,
-    TEAM_MEMBER
-}
